42ee9ebc6a12195eb45ada0424cb6b72805d8b1e
charalambosp <papaloizouc@gmail.com>
2014-06-14 19:40:20 +0100

    fixing neo4j

--- graph/lastfm.py
+++ graph/lastfm.py
@@ -2,30 +2,52 @@ import requests
 from pprint import pprint
 import os
 import pickle
-from neomodel import (
-    cypher_query, StringProperty, IntegerProperty, BooleanProperty, StructuredNode
-)
+from py2neo import neo4j
+from py2neo import cypher
+
+graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+
+artists = graph_db.get_or_create_index(neo4j.Node, "Artists")
 
 key = os.environ["LASTFM_KEY"]
 url = "http://ws.audioscrobbler.com/2.0"
 img_order = ['large', 'medium', 'extralarge', 'mega', 'small']
 cache = {}
 
+# people.create('id', '10', {"name": "Alice Smith 2"})
+# artist = artists.get("id", "10")[0]
+# artist.get_properties()
 
-class Artist(StructuredNode):
-    url = StringProperty(required=False, index=True)
-    mbid = StringProperty(required=True, unique_index=True)
-    img = StringProperty(required=False, index=False)
-    name = StringProperty(required=True, index=True)
-    visited = BooleanProperty(required=True, default=False)
+def get_all_artists():
+    cypher = """
+        START root=node:Artists("*:*")
+        RETURN root
+    """
+    query = neo4j.CypherQuery(graph_db, cypher)
+    return [i[0].get_properties() for i in query.execute()]
 
-    def __init__(self, url, mbid, name, img, visited=False, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+
+class Artist:
+    def __init__(self, url, mbid, name, image, visited):
         self.url = url
         self.mbid = mbid
-        self.img = img
+        self.image = image
         self.name = name
         self.visited = visited
+
+    def _args(self):
+        return {
+            "name": self.name,
+            "mbid": self.mbid,
+            "image": self.image,
+            "visited": self.visited,
+            "url": self.url
+        }
+
+    def get_or_create(self):
+        return artists.get_or_create('mbid',
+                                     self.mbid,
+                                     self._args())
 
     def __repr__(self):
         return " ".join((self.name, self.mbid, self.url, self.img))
@@ -70,10 +92,11 @@ def get_artist_related(artist):
 def parse_artist(row, match=True):
     args = row["url"], row["mbid"], row["name"], get_image(row["image"])
     url, mbid, name, image = args
-    artist = Artist(url, mbid, name, image, visited=True)
     if match:
+        artist = Artist(url, mbid, name, image, False)
         return row["match"], artist
     else:
+        artist = Artist(url, mbid, name, image, True)
         return artist
 
 
@@ -85,22 +108,33 @@ def fetch(artist):
     _artist = get_artist_info(artist)
     related = get_artist_related(artist)
     origin = parse_artist(_artist, match=False)
+    origin = origin.get_or_create()
     related = [parse_artist(i) for i in related]
 
     cache[artist] = (origin, related)
     return origin, related
 
 
-if __name__ == '__main__':
-    # database !?
+def load_cache():
+    cache = {}
     with open("cache.pickle", "rb") as f:
         cache = pickle.load(f)
-    origin, related = fetch("j.j. cale")
-    for i in related:
-        print(i[1].name)
-        fetch(i[1].name)
+    return cache
 
+
+def save_cache():
     with open("cache.pickle", "wb") as f:
         pickle.dump(cache, f)
 
-    pprint(cache)
+
+if __name__ == '__main__':
+    cache = load_cache()
+    origin, related = fetch("j.j. cale")
+
+    for i in related:
+        score, artist = i
+        artist = artist.get_or_create()
+        artist_props = artist.get_properties()
+        origin2, result2 = fetch(artist["name"])
+
+    save_cache()
