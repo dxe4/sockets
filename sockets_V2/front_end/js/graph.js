e9486b492ad13c95f5ba186646349cd59831c166
charalambosp <papaloizouc@gmail.com>
2014-06-15 06:55:02 +0100

    linting

--- sockets_V2/front_end/js/graph.js
+++ sockets_V2/front_end/js/graph.js
@@ -1,99 +1,99 @@
 /*var graph = 'data.json'
-    h = 700,
-    r = 10;
-    link_color = 'magenta';
-var zoom = d3.behavior.zoom().on("zoom", redraw);
+ h = 700,
+ r = 10;
+ link_color = 'magenta';
+ var zoom = d3.behavior.zoom().on("zoom", redraw);
 
-var vis = this.vis = d3.select("#holder")
-    .append("svg:svg")
-    .attr("width", w)
-    .attr("height", h)
-    .attr("pointer-events", "all")
-    .append('svg:g')
-    .call(zoom)
-    .append('svg:g');
+ var vis = this.vis = d3.select("#holder")
+ .append("svg:svg")
+ .attr("width", w)
+ .attr("height", h)
+ .attr("pointer-events", "all")
+ .append('svg:g')
+ .call(zoom)
+ .append('svg:g');
 
-vis.append('svg:rect')
-    .attr('width', w)
-    .attr('height', h)
-    .attr('fill', 'rgba(1,1,1,0)')
+ vis.append('svg:rect')
+ .attr('width', w)
+ .attr('height', h)
+ .attr('fill', 'rgba(1,1,1,0)')
 
-function redraw() {
-    vis.attr("transform","translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")"); } 
-    
-var force = d3.layout.force()
-    .gravity(0.01)
-    .charge(-400)
-    .size([w, h]);
+ function redraw() {
+ vis.attr("transform","translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")"); }
 
-var svg = d3.select(".text").append("svg")
-    .attr("width", w)
-    .attr("height", h);
-        
-d3.json(graph, function(json) {
-    var link = vis.selectAll("line")
-        .data(json.links)
-        .enter().append("line")
-        .attr("stroke-opacity", function(d) { return d.score/10; })
-        .attr("stroke-width", function(d) { return 6/d.score })
-        .style("stroke", link_color)
-        .on("mouseover", function(){d3.select(this).attr("stroke-opacity", "1.0");})
-        .on("mouseout", function(){
-            d3.select(this).attr("stroke-opacity", 
-                function(d) { return d.score/10;})
-            .attr("stroke-width", function(d) { return 6/d.score }) 
-        });
+ var force = d3.layout.force()
+ .gravity(0.01)
+ .charge(-400)
+ .size([w, h]);
 
-        link.append("title")
-            .text(function(d) { return d.score } );         
+ var svg = d3.select(".text").append("svg")
+ .attr("width", w)
+ .attr("height", h);
 
-    var node = vis.selectAll("g.node")
-        .data(json.nodes)
-        .enter().append("svg:g")
-        .attr("class","node")
-        .call(force.drag);
-        
-        node.append("svg:circle")
-            .attr("r", 50)
-            .style("fill", function(d) { if(d.style == 'filled') { return d.color;}; })
-            .style("stroke", function(d) { if(d.style !== 'filled') { return d.color;}; })
-            .style("stroke-width", "4")
-            .on("mouseover", function(){d3.select(this).style("fill", "#999");})
-            .on("mouseout", function(d) {
-                if (d.style == 'filled') { d3.select(this).style("fill",d.color); }
-                else {
-                d3.select(this).style("stroke",d.color);
-                d3.select(this).style("fill","black");
-                } } );
-            
-        node.append("svg:text")
-            .attr("text-anchor", "middle") 
-            .attr("fill","#fff")
-            .style("pointer-events", "none")
-            .attr("font-size", '16px' )
-            .text( function(d) { return d.name; } ) ;
-            
-        node.append("title")
-            .text(function(d) { return d.URI } );
+ d3.json(graph, function(json) {
+ var link = vis.selectAll("line")
+ .data(json.links)
+ .enter().append("line")
+ .attr("stroke-opacity", function(d) { return d.score/10; })
+ .attr("stroke-width", function(d) { return 6/d.score })
+ .style("stroke", link_color)
+ .on("mouseover", function(){d3.select(this).attr("stroke-opacity", "1.0");})
+ .on("mouseout", function(){
+ d3.select(this).attr("stroke-opacity",
+ function(d) { return d.score/10;})
+ .attr("stroke-width", function(d) { return 6/d.score })
+ });
 
-    force
-        .nodes(json.nodes)
-        .links(json.links)
-        .on("tick", tick)
-        .start();
-            
-  function tick() {
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; })
-        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")";});
+ link.append("title")
+ .text(function(d) { return d.score } );
 
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-  }
-});
-*/
+ var node = vis.selectAll("g.node")
+ .data(json.nodes)
+ .enter().append("svg:g")
+ .attr("class","node")
+ .call(force.drag);
+
+ node.append("svg:circle")
+ .attr("r", 50)
+ .style("fill", function(d) { if(d.style == 'filled') { return d.color;}; })
+ .style("stroke", function(d) { if(d.style !== 'filled') { return d.color;}; })
+ .style("stroke-width", "4")
+ .on("mouseover", function(){d3.select(this).style("fill", "#999");})
+ .on("mouseout", function(d) {
+ if (d.style == 'filled') { d3.select(this).style("fill",d.color); }
+ else {
+ d3.select(this).style("stroke",d.color);
+ d3.select(this).style("fill","black");
+ } } );
+
+ node.append("svg:text")
+ .attr("text-anchor", "middle")
+ .attr("fill","#fff")
+ .style("pointer-events", "none")
+ .attr("font-size", '16px' )
+ .text( function(d) { return d.name; } ) ;
+
+ node.append("title")
+ .text(function(d) { return d.URI } );
+
+ force
+ .nodes(json.nodes)
+ .links(json.links)
+ .on("tick", tick)
+ .start();
+
+ function tick() {
+ node.attr("cx", function(d) { return d.x; })
+ .attr("cy", function(d) { return d.y; })
+ .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")";});
+
+ link.attr("x1", function(d) { return d.source.x; })
+ .attr("y1", function(d) { return d.source.y; })
+ .attr("x2", function(d) { return d.target.x; })
+ .attr("y2", function(d) { return d.target.y; });
+ }
+ });
+ */
 
 var w = document.getElementById('holder_container').offsetWidth;
 var store = [];
@@ -105,47 +105,51 @@ function myGraph(el) {
     this.addNode = function (obj) {
         nodes.push(obj);
         update();
-    }
+    };
 
     this.removeNode = function (id) {
         var i = 0;
         var n = findNode(id);
         while (i < links.length) {
-            if ((links[i]['source'] === n)||(links[i]['target'] == n)) links.splice(i,1);
+            if ((links[i]['source'] === n) || (links[i]['target'] == n)) links.splice(i, 1);
             else i++;
         }
         var index = findNodeIndex(id);
-        if(index !== undefined) {
+        if (index !== undefined) {
             nodes.splice(index, 1);
             update();
         }
-    }
+    };
 
     this.addLink = function (sourceId, targetId, score) {
         var sourceNode = findNode(sourceId);
         var targetNode = findNode(targetId);
 
-        if((sourceNode !== undefined) && (targetNode !== undefined)) {
+        if ((sourceNode !== undefined) && (targetNode !== undefined)) {
             links.push({"source": sourceNode, "target": targetNode, 'score': score});
             update();
         }
-    }
+    };
 
     var findNode = function (id) {
-        for (var i=0; i < nodes.length; i++) {
+        for (var i = 0; i < nodes.length; i++) {
             if (nodes[i].id === id)
                 return nodes[i]
-        };
-    }
+        }
+
+    };
 
     var findNodeIndex = function (id) {
-        for (var i=0; i < nodes.length; i++) {
+        for (var i = 0; i < nodes.length; i++) {
             if (nodes[i].id === id)
                 return i
-        };
-    }
+        }
+
+    };
+
     function redraw() {
-        vis.attr("transform","translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")"); } 
+        vis.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+    };
 
     var zoom = d3.behavior.zoom().on("zoom", redraw);
 
@@ -181,33 +185,45 @@ function myGraph(el) {
         .attr("height", h);
 
 
-
     var update = function () {
 
         var link = vis.selectAll("line.link")
-            .data(links, function(d) { return d.source.id + "-" + d.target.id; });
+            .data(links, function (d) {
+                return d.source.id + "-" + d.target.id;
+            });
 
         link.enter()
             .insert("line")
-            .attr("stroke-opacity", function(d) { return d.score; })
-            .attr("stroke-width", function(d) { return d.score*5; })
+            .attr("stroke-opacity", function (d) {
+                return d.score;
+            })
+            .attr("stroke-width", function (d) {
+                return d.score * 5;
+            })
             .attr("class", "link")
-            .on("mouseover", function(){
+            .on("mouseover", function () {
                 d3.select(this)
-                .attr("stroke-opacity", "1.0")
-                .attr("stroke-width", 5) 
-            ;})
+                    .attr("stroke-opacity", "1.0")
+                    .attr("stroke-width", 5)
+                ;
+            })
 
-            .on("mouseout", function(){
+            .on("mouseout", function () {
                 d3.select(this)
-                .attr("stroke-opacity", function(d) { return d.score;})
-                .attr("stroke-width", function(d) { return d.score*5; }) 
+                    .attr("stroke-opacity", function (d) {
+                        return d.score;
+                    })
+                    .attr("stroke-width", function (d) {
+                        return d.score * 5;
+                    })
             });
 
         link.exit().remove();
 
         var node = vis.selectAll("g.node")
-            .data(nodes, function(d) { return d.id;});
+            .data(nodes, function (d) {
+                return d.id;
+            });
 
         var nodeEnter = node.enter().append("g")
             .attr("class", "node")
@@ -220,7 +236,9 @@ function myGraph(el) {
 
         nodeEnter.append("image")
             .attr("class", "circle")
-            .attr("xlink:href", function(d){ return d.image })
+            .attr("xlink:href", function (d) {
+                return d.image
+            })
             .attr("x", "-8px")
             .attr("y", "-8px")
             .attr("width", "16px")
@@ -230,24 +248,36 @@ function myGraph(el) {
             .attr("class", "nodetext")
             .attr("dx", 12)
             .attr("dy", ".35em")
-            .text(function(d) {return d.name});
+            .text(function (d) {
+                return d.name
+            });
 
         node.exit().remove();
 
-        force.on("tick", function() {
-          link.attr("x1", function(d) { return d.source.x; })
-              .attr("y1", function(d) { return d.source.y; })
-              .attr("x2", function(d) { return d.target.x; })
-              .attr("y2", function(d) { return d.target.y; });
+        force.on("tick", function () {
+            link.attr("x1", function (d) {
+                return d.source.x;
+            })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
 
-          node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+            node.attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
         });
 
         // Restart the force layout.
         force.start();
-    }
+    };
 
-    this.zoom = function(direction){
+    this.zoom = function (direction) {
         if (direction == "in") {
             var scale = zoom.scale() * 1.5;
             var x = ((zoom.translate()[0] - (w / 2)) * 1.5) + w / 2;
@@ -258,14 +288,14 @@ function myGraph(el) {
             var y = ((zoom.translate()[1] - (h / 2)) * .75) + h / 2;
         }
         this.scale = scale;
-        zoom.scale(scale).translate([x,y])
-        vis.transition().duration(750).attr("transform","translate(" + x +',' + y + ")" + " scale(" + scale + ")");
-    }
-    this.translate = function(x, y){
+        zoom.scale(scale).translate([x, y])
+        vis.transition().duration(750).attr("transform", "translate(" + x + ',' + y + ")" + " scale(" + scale + ")");
+    };
+    this.translate = function (x, y) {
         var scale = zoom.scale();
-        zoom.translate([x,y]);
-        vis.transition().duration(750).attr("transform","translate(" + x +',' + y + ")" + " scale(" + scale + ")");
-    }
+        zoom.translate([x, y]);
+        vis.transition().duration(750).attr("transform", "translate(" + x + ',' + y + ")" + " scale(" + scale + ")");
+    };
 
     this.nodes = nodes;
 
@@ -277,22 +307,22 @@ graph = new myGraph("#holder");
 
 // You can do this from the console as much as you like...
 
-function filterJSONandAddToGraph(JSONResponse){
+function filterJSONandAddToGraph(JSONResponse) {
     console.log(typeof(JSONResponse));
     JSONResponse = $.parseJSON(JSONResponse);
     console.log(JSONResponse);
     var mbid = JSONResponse[0].id;
     graph.addNode(JSONResponse[0])
 
-    store.push(JSONResponse[0]);   
-    for(var i = 1; i < JSONResponse[1].length; i++){
-        
-            console.log(JSONResponse[1][i]);
-            store.push(JSONResponse[1][i][1]);
-            JSONResponse[1][i][1].id = JSONResponse[1][i][1].name;
-            graph.addNode(JSONResponse[1][i][1]);
-            console.log(JSONResponse[1][i]);
-            graph.addLink(JSONResponse[0].id, JSONResponse[1][i][1].id, JSONResponse[1][i][0]);
+    store.push(JSONResponse[0]);
+    for (var i = 1; i < JSONResponse[1].length; i++) {
+
+        console.log(JSONResponse[1][i]);
+        store.push(JSONResponse[1][i][1]);
+        JSONResponse[1][i][1].id = JSONResponse[1][i][1].name;
+        graph.addNode(JSONResponse[1][i][1]);
+        console.log(JSONResponse[1][i]);
+        graph.addLink(JSONResponse[0].id, JSONResponse[1][i][1].id, JSONResponse[1][i][0]);
     }
 
 }
