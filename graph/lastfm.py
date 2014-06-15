9792df47acde4d08e5af3304b46f644c7ac17090
charalambosp <papaloizouc@gmail.com>
2014-06-15 04:34:49 +0100

    origin=*

--- graph/lastfm.py
+++ graph/lastfm.py
@@ -9,6 +9,10 @@ import py2neo
 import time
 from flask import Flask, jsonify, request, Response
 
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
+
 graph_db = neo4j.GraphDatabaseService("http://127.0.0.1:7474/db/data/")
 
 artists = graph_db.get_or_create_index(neo4j.Node, "Artists")
@@ -16,6 +20,53 @@ artists = graph_db.get_or_create_index(neo4j.Node, "Artists")
 key = os.environ["LASTFM_KEY"]
 url = "http://ws.audioscrobbler.com/2.0"
 img_order = ['large', 'medium', 'extralarge', 'mega', 'small']
+
+
+
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, str):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, str):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+
 
 # people.create('id', '10', {"name": "Alice Smith 2"})
 # artist = artists.get("id", "10")[0]
@@ -193,6 +244,7 @@ def run():
 app = Flask(__name__)
 
 @app.route('/get_related', methods=["POST"])
+@crossdomain(origin="*")
 def get_related():
     artist = request.form['artist']
     cypher = """
